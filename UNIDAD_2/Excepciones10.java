cdecb8ecaff127d192dbea3fb7577b15adcd3a97
rosilloartechefernanda <133398320+rosilloartechefernanda@users.noreply.github.com>
2023-07-03 10:33:21 -0600

    Add files via upload

--- UNIDAD_2/Excepciones10.java
+++ UNIDAD_2/Excepciones10.java
@@ -1,21 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package excepciones10;
 
-/**
- *
- * @author plloo
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class Excepciones10 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+         //IOException
+       BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("nonexistentfile.txt"));
+            String line = reader.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            System.out.println("¡Error: se produjo una excepción de E/S!");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
 }
